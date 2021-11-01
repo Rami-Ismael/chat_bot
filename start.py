849de201bd462e6a428a323d02856bcc6e7a2d63
Rami-Ismael <rami.m.ismael@gmail.com>
2021-11-01 07:01:24 -0500

    Ask a diferent question

--- start.py
+++ start.py
@@ -8,6 +8,9 @@ assert subscription_key
 search_url = "https://api.bing.microsoft.com/v7.0/search"
 from bs4 import BeautifulSoup
 import requests
+## a method that takes in a list of string and return a single string
+def list_to_string(list_of_string):
+    return " ".join(list_of_string)
 
 ## determine if stuff's elements exist in stuffa
 def check_list(sentence, stuffa):
@@ -53,6 +56,12 @@ def get_text_of_website(website_url):
         page = requests.get(website_url)
         soup = BeautifulSoup(page.content, 'html.parser')
         text=  soup.get_text()
+        ##strip the website of javasript function
+        text = text.replace("document.write(", "")
+        ##strip the website text of javasript function and mthod function
+        text = re.sub("\w+\(.*\)", "", text)
+        ## strip the website text of javascript and html tags
+        text = re.sub("<[^>]+>", "", text)
         ## preprocess beautiful soap to a single text string
         text_list = text.split("\n")
         ## remove all space size greater than one from a list of string 
@@ -63,23 +72,15 @@ def get_text_of_website(website_url):
         text_list = [element for element in text_list if len(element)>20]
         ## remove all element that have word from the list of string useless from a list of string
         text_list = [element for element in text_list if check_list(element , useless)]
-        return text_list
+        return list_to_string(text_list)
     except:
         return "Error"
-## a method that takes in a list of string and return a single string
-def list_to_string(list_of_string):
-    return " ".join(list_of_string)
 
 
 
 
 
-text = get_text_of_website( search_results["webPages"]["value"][0]["url"] )
-print( text)
-print(len( text ) )
-text = list_to_string(text)
-print( len ( text ) )
-print( text )
+
 ###
 
 '''
@@ -91,7 +92,6 @@ question_answer = pipeline("question-answering")
 
 result  = question_answer( question = question_text , context = text )
 print(f"Answer: '{result['answer']}', score: {round(result['score'], 4)}, start: {result['start']}, end: {result['end']}")
-'''
 from transformers import LongformerTokenizer, LongformerForQuestionAnswering
 
 tokenizer = LongformerTokenizer.from_pretrained("allenai/longformer-large-4096-finetuned-triviaqa")
@@ -112,5 +112,31 @@ for question in questions:
 
     answer_text = all_token[torch.argmax(start_logit) : torch.argmax(end_logit)+1]
     answer = tokenizer.decode( tokenizer.convert_tokens_to_ids(answer_text) ) # remove space prepending space token
+'''
 
+print( get_text_of_website("https://gameofthrones.fandom.com/wiki/Mutiny_at_Castle_Black"))
 
+from transformers import LongformerTokenizer, LongformerForQuestionAnswering
+import torch
+
+tokenizer = LongformerTokenizer.from_pretrained("allenai/longformer-large-4096-finetuned-triviaqa")
+model = LongformerForQuestionAnswering.from_pretrained("allenai/longformer-large-4096-finetuned-triviaqa")
+import math
+question, text = "How does Jon Snow die?", get_text_of_website("https://gameofthrones.fandom.com/wiki/Jon_Snow")
+for i in range(0, math.ceil(len(text)/4096) ):
+
+    encoding = tokenizer(question, text[(i-1)*4096:i*4096], return_tensors="pt")
+    input_ids = encoding["input_ids"]
+
+    # default is local attention everywhere
+    # the forward method will automatically set global attention on question tokens
+    attention_mask = encoding["attention_mask"]
+
+    outputs = model(input_ids, attention_mask=attention_mask)
+    start_logits = outputs.start_logits
+    end_logits = outputs.end_logits
+    all_tokens = tokenizer.convert_ids_to_tokens(input_ids[0].tolist())
+
+    answer_tokens = all_tokens[torch.argmax(start_logits) :torch.argmax(end_logits)+1]
+    answer = tokenizer.decode(tokenizer.convert_tokens_to_ids(answer_tokens)) # remove space prepending space token
+    print( answer )
